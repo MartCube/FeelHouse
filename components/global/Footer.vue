7524cf488532bdcab1189e808641df97538af344
Maryanushka <marya.vadymovna@gmail.com>
2022-01-27 17:50:58 +0200

    footer desktop + mob

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,43 +1,33 @@
 <template>
 	<footer class="main-footer dark">
 		<div class="topbanner-footer">
-			<div class="section-padding banner-img valign bg-img bg-fixed" style="background-image: url('1.jpg')">
+			<div class="section-padding banner-img bg-img bg-fixed" style="background-image: url('/1.jpg')">
 				<div class="container">
 					<div class="section-title">Contact Us</div>
-					<div class="row">
+					<div class="info">
 						<div class="phones">
 							<h6>Phone</h6>
-							<h5 class="mb-30">+1 123-456-0606</h5>
-							<p class="mb-30">Monday - Friday: 8am - 6pm<br />Saturday - Sunday: 9am - 3pm</p>
+							<h5><a href="tel:1 123-456-0606">+1 123-456-0606</a></h5>
+							<p>Monday - Friday: 8am - 6pm<br />Saturday - Sunday: 9am - 3pm</p>
 						</div>
 						<div class="adress">
 							<h6>Email</h6>
-							<h5 class="mb-30">info@architecture.com</h5>
-							<p class="mb-30">24 King St, Charleston<br />SC 29401 USA</p>
+							<h5><a href="mailto:info@architecture.com">info@architecture.com</a></h5>
+							<p>24 King St, Charleston<br />SC 29401 USA</p>
 						</div>
 						<div class="vid-area">
-							<a class="play-button" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.758017847153!2d-79.93420398486563!3d32.77215479154045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88fe7a1ae84ff639%3A0xe5c782f71924a526!2s24%20King%20St%2C%20Charleston%2C%20SC%2029401%2C%20USA!5e0!3m2!1sen!2str!4v1631170502143!5m2!1sen!2str">
-								<svg class="circle-fill">
-									<circle cx="43" cy="43" r="39" stroke="#272727" stroke-width="1"></circle>
-								</svg>
-								<svg class="circle-track">
-									<circle cx="43" cy="43" r="39" stroke="none" stroke-width="1" fill="none"></circle>
-								</svg>
-								<span class="polygon">
-									<Icon name="location" :fill="color" />
-								</span>
+							<a class="location_button" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.758017847153!2d-79.93420398486563!3d32.77215479154045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88fe7a1ae84ff639%3A0xe5c782f71924a526!2s24%20King%20St%2C%20Charleston%2C%20SC%2029401%2C%20USA!5e0!3m2!1sen!2str!4v1631170502143!5m2!1sen!2str">
+								<Icon name="location" :fill="color" size="3rem" />
 							</a>
-							<div class="cont mt-20 mb-30">
-								<h5>Our Location</h5>
-							</div>
+							<h5>Our Location</h5>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="container">
+		<div class="container bottom-footer">
 			<div class="item abot">
-				<div class="logo mb-15"><img src="logo-light.png" alt="" /></div>
+				<div class="logo"><img src="/logo-light.png" alt="" /></div>
 				<p>Welcome to our FeelHouse Architecture agency. Viverra tristique justo in duis vitae diam neque niva.</p>
 				<div class="social-icon">
 					<a href="#">
@@ -68,8 +58,8 @@
 				</div>
 				<p>Have a project in mind?</p>
 				<p>Let's Build Something Great Together</p>
-				<p>+1 123-456-0606</p>
-				<p>info@architecture.com</p>
+				<a href="tel:1 123-456-0606">+1 123-456-0606</a>
+				<a href="mailto:info@architecture.com">info@architecture.com</a>
 			</div>
 		</div>
 		<div class="sub-footer">
@@ -93,15 +83,21 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.bottom-footer {
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	.item {
+		width: 20%;
+	}
+	.logo {
+		margin-bottom: 2rem;
+	}
+}
 .bg-img {
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: center;
-}
-.bg-img2 {
-	background-size: contain;
-	background-repeat: no-repeat;
-	background-position: left;
 }
 .bg-fixed {
 	background-attachment: fixed;
@@ -111,51 +107,102 @@ export default {
 	.container {
 		display: flex;
 		justify-content: space-between;
+		flex-wrap: wrap;
+		z-index: 3;
+		position: relative;
+		.section-title {
+			width: 100%;
+		}
+		.info {
+			margin-top: 3rem;
+			width: 100%;
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+			.phones,
+			.vid-area,
+			.adress {
+				width: 20%;
+			}
+			.vid-area {
+				a {
+					display: flex;
+					position: relative;
+					width: 120px;
+					height: 120px;
+					z-index: 1;
+					background-color: rgb(255, 255, 255, 50%);
+					border: 2px solid $text;
+					border-width: 0;
+					transition: border 0.5s ease-in;
+					border-radius: 50%;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					&:hover {
+						animation: dash 5s linear forwards;
+					}
+				}
+			}
+		}
+	}
+}
+@keyframes dash {
+	from {
+		border-width: 0;
+	}
+	to {
+		border-width: 100%;
 	}
 }
 .topbanner-footer h6 {
-	color: #fff;
 	font-size: 17px;
-	font-family: 'Khand', sans-serif;
 	font-weight: 400;
 	margin-bottom: 10px;
 	text-transform: uppercase;
 	letter-spacing: 4px;
 }
 .topbanner-footer h5 {
-	color: #fff;
 	font-size: 24px;
-	font-family: 'Khand', sans-serif;
-	margin-bottom: 20px;
+	margin: 20px 0;
 }
 .topbanner-footer h5 a {
-	color: #c5a47e;
+	color: $primary;
 }
 .topbanner-footer h1 {
 	font-size: 45px;
-	font-family: 'Khand', sans-serif;
-	color: #fff;
 	position: relative;
 	margin-bottom: 20px;
 	line-height: 1.25em;
 }
 .topbanner-footer p {
 	font-size: 17px;
-	font-weight: 400;
-	color: #fff;
 	line-height: 1.75em;
 }
 @media screen and (max-width: 800px) {
 	.topbanner-footer {
+		.section-padding{
+			padding: 0 1rem;
+		}
+		.container .info {
+			.phones,
+			.vid-area,
+			.adress {
+				width: 100%;
+				margin-top: 2rem;
+			}
+		}
+		h6 {
+			font-size: 12px;
+			margin-bottom: 10px;
+		}
 	}
-	.topbanner-footer h6 {
-		font-size: 12px;
-		font-weight: 400;
-		margin-bottom: 10px;
-	}
-	.topbanner-footer h5 {
-	}
-	.topbanner-footer h1 {
+	.bottom-footer {
+		padding: 0 1rem;
+		.item {
+			width: 100%;
+			margin-top: 2rem;
+		}
 	}
 }
 .banner-img:after {
@@ -165,21 +212,31 @@ export default {
 	left: 0;
 	top: 0;
 	content: '';
-	background: -webkit-linear-gradient(bottom, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%, rgba(255, 255, 255, 0.2) 100%);
-	background: linear-gradient(to top, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%, rgba(255, 255, 255, 0.2) 100%);
+	background: -webkit-linear-gradient(bottom, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%);
+	background: linear-gradient(to top, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%);
 	object-fit: cover;
 }
 .main-footer {
 	padding: 0;
+	&::before {
+		height: 80px;
+		width: 2px;
+		background-color: $primary;
+		content: '';
+		z-index: 10;
+		top: -40px;
+		left: 50%;
+		position: absolute;
+		transform: translateX(-50%);
+	}
 }
 .main-footer.dark {
 	position: relative;
-	background: #fff;
-	color: #555;
+	color: $text_light;
 }
 .main-footer p,
 .main-footer .abot p {
-	color: #555;
+	color: $text_light;
 }
 .main-footer .abot .logo img {
 	width: 150px;
@@ -198,7 +255,7 @@ export default {
 	color: rgba(255, 255, 255, 0.7);
 }
 .main-footer .abot .social-icon a:hover {
-	color: #c5a47e;
+	color: $primary;
 }
 .main-footer .usful-links ul {
 	margin: 0;
@@ -207,65 +264,60 @@ export default {
 .main-footer .usful-links ul li {
 	margin-bottom: 5px;
 }
-.main-footer .usful-links ul li:hover {
-	color: #fff;
-}
 .main-footer .usful-links ul li i {
 	font-size: 7px;
-	color: #fff;
 	margin-right: 3px;
 }
-.main-footer .usful-links ul li a {
-	color: rgba(255, 255, 255, 0.7);
-}
 .main-footer .usful-links ul li a:hover {
-	color: #c5a47e;
+	color: $primary;
 }
 .main-footer .fothead h6 {
-	color: #fff;
 	font-size: 30px;
 	text-transform: uppercase;
 	letter-spacing: 1px;
 	margin-bottom: 15px;
 	font-weight: 500;
+	color: $text_light;
 }
 .main-footer .fotcont .fothead h6 {
 	position: relative;
 }
 .main-footer .fotcont p {
 	margin-bottom: 0px;
-	color: rgba(255, 255, 255, 0.7);
+	color: $text_light;
 }
 .main-footer .sub-footer {
-	padding: 20px 0;
+	padding: 20px 1rem;
 	border-top: 1px solid #f6f6f6;
+	margin-top: 2rem;
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-wrap: wrap;
+	}
 }
 .main-footer .sub-footer p {
 	font-size: 15px;
-	color: #555;
+	color: $text_light;
 	margin-bottom: 0;
 }
-.main-footer .sub-footer p a {
-	color: rgba(255, 255, 255, 0.7);
-}
 .main-footer .sub-footer p a:hover {
-	color: #c5a47e;
+	color: $primary;
 }
 .main-footer .sub-footer p span {
-	color: rgba(255, 255, 255, 0.1);
 	padding: 0 10px;
 }
 .main-footer .logo h2 {
 	font-weight: normal;
 	font-size: 30px;
-	color: #fff;
+	color: $white;
 	line-height: 1.25em;
 	margin-bottom: 20px;
 	letter-spacing: 1px;
 }
 .main-footer .logo span {
 	display: block;
-	color: #fff;
 	text-transform: uppercase;
 	letter-spacing: 3px;
 	font-size: 9px;
--- components/sections/Intro.vue
+++ components/sections/Intro.vue
@@ -33,7 +33,17 @@ export default {
 	display: flex;
 	align-items: center;
 	justify-content: center;
-
+	&::after {
+		height: 80px;
+		width: 2px;
+		background-color: $primary;
+		content: '';
+		z-index: 10;
+		bottom: -40px;
+		left: 50%;
+		position: absolute;
+		transform: translateX(-50%);
+	}
 	.box {
 		width: 100%;
 		height: 100%;
